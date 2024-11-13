43369eae9f9aff4e377069dfdcdafbe2be94cc3f
noiodiamoelia <ginevra.fogli@iismontalegenova.it>
2024-11-13 13:38:07 +0100

    disegno

---
+++ programma.js
@@ -0,0 +1,13 @@
+
+
+let foglio = document.getElementById("foglio")
+
+let disegno = foglio.getContext("2d")
+
+disegno.fillStyle = "blue"
+
+disegno.fillRect(0,0, 100, 50)
+
+disegno.fillStyle = "green"
+
+disegno.fillRect(30, 30, 100, 50)

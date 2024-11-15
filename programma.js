af8de4694c304893f42b5ed7bfc2cf2a03728b58
noiodiamoelia <ginevra.fogli@iismontalegenova.it>
2024-11-15 10:36:01 +0100

    ttt

--- programma.js
+++ programma.js
@@ -6,8 +6,29 @@ let disegno = foglio.getContext("2d")
 
 disegno.fillStyle = "blue"
 
-disegno.fillRect(0,0, 100, 50)
+disegno.fillRect(0,0, 200, 100)
 
-disegno.fillStyle = "green"
+disegno.fillStyle = "orange"
 
-disegno.fillRect(30, 30, 100, 50)
+disegno.fillRect(30, 80, 200, 100)
+
+disegno.fillStyle = "black"
+
+disegno.fillRect(250, 0, 10, 500)
+
+disegno.fillStyle = "pink"
+
+disegno.fillRect(50,150, 200, 100)
+
+disegno.fillStyle = "purple"
+
+disegno.fillRect(200, 200, 200, 500)
+
+let foglio2 = document.getElementById("foglio2")
+
+let disegno2 = foglio2.getContext("2d")
+
+foglio2.addEventListener("click", (evento) => {
+    disegno2.strokeRect(evento.offsetX,evento.offsetY,500*Math.random(),500*Math.random())
+
+})
